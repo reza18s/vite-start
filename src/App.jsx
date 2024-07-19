854715b1e0672071f4bfcfc4fdec97531d1e3d1a
reza18s <swary2021@gmail.com>
2024-07-19 14:51:17 +0330

    add

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-
-import * as sentry from "@sentry/react";
-
-function App() {
-   return <div></div>;
-}
-
-export default sentry.withProfiler(App);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        blue: "#2997FF",
-        gray: {
-          DEFAULT: "#86868b",
-          100: "#94928d",
-          200: "#afafaf",
-          300: "#42424570",
-        },
-        zinc: "#101010",
-      },
-    },
-  },
-  plugins: [],
-};
-=======
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -95,4 +75,3 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 }
->>>>>>> fc15bd1 (add)
